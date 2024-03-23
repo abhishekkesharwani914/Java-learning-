5cd5b7d252dc5eb2f63aef111fe2e22548209fc9
Abhishek Kesharwani <abhishekkesharwani914@gmail.com>
2024-03-23 13:56:02 +0530

    adding some new patters

--- patterns.java
+++ patterns.java
@@ -4,146 +4,196 @@ public class patterns {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // int m = sc.nextInt();
+        int m = sc.nextInt();
 
         // 1.) Solid Rectangle Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= m; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 2.) Hollow Rectangle Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= m; j++) {
-        // if (i == 1 || j == 1 || i == n || j == m) {
-        // System.out.print("* ");
-        // } else {
-        // System.out.print(" ");
-        // }
-        // }
-        // System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
         // 3.) Half Pyramid Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 4.) Inverted Half Pyramid Pattern
-        // for (int i = n; i >= 1; i--) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 5.) Half Pyramid Pattern Other Side Rotated by 180 degree
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= (n - i); j++) {
-        // System.out.print(" ");
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
 
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 6.) Full Pyramid Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= (n - i); j++) {
-        // System.out.print(" ");
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
 
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("* ");
-        // }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
 
-        // for (int j = 2; j <= i; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+            for (int j = 2; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 7.) Print the Pyramid Pattern with no. start from 1 to n.
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(j + " ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
         // 8.) Print the Inverted Pyramid with no. start from 1 to n
-        // for (int i = n; i >= 1; i--) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(j + " ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
         // 9.) Floyd's Triangle Pattern
-        // int num1 = 1;
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(num1 + " ");
-        // num++;
-        // }
-        // System.out.println();
-        // }
+        int num1 = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num1 + " ");
+                num1++;
+            }
+            System.out.println();
+        }
 
         // 10.) 0-1 Triangle Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // int sum = i + j;
-        // if (sum % 2 == 0) {
-        // System.out.print("1 ");
-        // } else {
-        // System.out.print("0 ");
-        // }
-        // }
-        // System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
 
         // 11.) Solid Rhombus Pattern
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= (n - i); j++) {
-        // System.out.print(" ");
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
 
-        // for (int j = 1; j <= n; j++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // 12.) Number Pyramid Pattern
-        // int num2 = 1;
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= (n - i); j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(num2 + " ");
-        // }
-        // num2++;
-        // System.out.println();
-        // }
+        int num2 = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num2 + " ");
+            }
+            num2++;
+            System.out.println();
+        }
 
         // 13.) Palindromic Number Pyramid Pattern.
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
             for (int j = i; j >= 1; j--) {
                 System.out.print(j + " ");
             }
             for (int j = 2; j <= i; j++) {
                 System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        // 14.) Solid Diamond Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            int stars = 2 * i - 1;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            int stars = 2 * i - 1;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 15.) Butterfly Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
