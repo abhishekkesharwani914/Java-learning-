01672b7f9069d37a56a494cf5c7bd9999993927a
Abhishek Kesharwani <137091837+abhishekkesharwani914@users.noreply.github.com>
2024-04-07 23:24:49 +0530

    New lines of code

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -8,6 +8,7 @@ public class PrimeNumber {
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         // Prime number upto range
+        System.out.println("Prime number upto range " + n);
         for (int i = 1; i <= n; i++) {
             boolean flag = false;
             for (int j = 2; j <= i / 2; j++) {
@@ -23,21 +24,22 @@ public class PrimeNumber {
             // System.out.println(i + " is not a prime number");
             // }
         }
-
+        System.out.println();
+        System.out.println("Given number is prime or not");
         // Given number is prime or not
+        boolean flg = false;
         for (int i = 1; i <= n / 2; i++) {
-            boolean flag = false;
             for (int j = 2; j <= i / 2; j++) {
                 if (i % j == 0) {
-                    flag = true;
+                    flg = true;
                     break;
                 }
             }
-            if (!flag) {
-                System.out.println(i + " is a prime number");
-            } else {
-                System.out.println(i + " is not a prime number");
-            }
+        }
+        if (!flg) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
         }
         sc.close();
     }
